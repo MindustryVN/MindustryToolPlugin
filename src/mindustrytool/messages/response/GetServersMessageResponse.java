8a72f3f49f84388d68f79949eeb8fbf96d2ddbb1
Sharlotte <66898063+sharrlotte@users.noreply.github.com>
2025-01-28 21:53:44 +0700

    update GetServersMessageResponse to include userId, hostCommand, status, official flag, ramUsage, totalRam, and change players to long

--- src/mindustrytool/messages/response/GetServersMessageResponse.java
+++ src/mindustrytool/messages/response/GetServersMessageResponse.java
@@ -3,6 +3,7 @@ package mindustrytool.messages.response;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 @Accessors(chain = true)
@@ -13,13 +14,19 @@ public class GetServersMessageResponse {
     @Data
     @Accessors(chain = true)
     public static class ResponseData {
-        private String id;
-        private String name;
-        private String description;
-        private String mode;
-        private int port;
-        private int players;
-        private String mapName;
+        public UUID id;
+        public UUID userId;
+        public String name;
+        public String description;
+        public String mode;
+        public int port;
+        public String hostCommand;
+        public String status = "DOWN";
+        public boolean official;
+        public long ramUsage;
+        public long totalRam;
+        public long players;
+        public String mapName;
         private List<String> mods;
     }
 }

4af2805fec6de7a344313dd5e930b4db43c9dfec
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-03-04 21:58:54 +0700

    fix(EventHandler): optimize message translation execution and improve error handling

--- src/mindustrytool/handlers/EventHandler.java
+++ src/mindustrytool/handlers/EventHandler.java
@@ -287,23 +287,22 @@ public class EventHandler {
             }
         });
 
-        executor.execute(() -> {
-
-            Groups.player.each(p -> {
-                if (p.id != player.id) {
-                    var locale = p.locale();
-                    try {
+        Groups.player.each(p -> {
+            if (p.id != player.id) {
+                var locale = p.locale();
+                try {
+                    executor.execute(() -> {
                         var translatedMessage = translationCache.computeIfAbsent(locale,
                                 key -> MindustryToolPlugin.apiGateway.translate(message, locale));
                         p.sendMessage("Translation: " + translatedMessage, player);
-                    } catch (Exception e) {
-                        Log.err(e);
-                    }
+                    });
+                } catch (Exception e) {
+                    Log.err(e);
                 }
-            });
-            translationCache.clear();
-
+            }
         });
+        translationCache.clear();
+
     }
 
     public void onPlayerLeave(PlayerLeave event) {

e2c0063147464aca7a597bf238977fa6fa146a41
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-05-16 10:33:46 +0700

    refactor(HttpServer): remove unused map and mod endpoints
    The map and mod endpoints in HttpServer.java were unused and added unnecessary complexity. This commit removes them to simplify the codebase and improve maintainability.

--- src/mindustrytool/handlers/HttpServer.java
+++ src/mindustrytool/handlers/HttpServer.java
@@ -10,12 +10,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 
 import arc.Core;
 import arc.files.Fi;
-import arc.files.ZipFi;
 import arc.struct.Seq;
-import arc.struct.StringMap;
 import arc.util.Log;
 import mindustry.Vars;
-import mindustry.core.Version;
 import mindustry.game.Gamemode;
 import mindustry.gen.Call;
 import mindustry.gen.Groups;
@@ -23,20 +20,15 @@ import mindustry.gen.Player;
 import mindustry.io.MapIO;
 import mindustry.maps.Map;
 import mindustry.maps.MapException;
-import mindustry.mod.Mods.ModLoadException;
-import mindustry.mod.Mods.ModMeta;
 import mindustry.net.Administration.PlayerInfo;
 import mindustrytool.Config;
 import mindustrytool.MindustryToolPlugin;
-import mindustrytool.type.MapDto;
 import mindustrytool.type.MindustryPlayerDto;
-import mindustrytool.type.ModDto;
 import mindustrytool.type.PlayerDto;
 import mindustrytool.type.ServerCommandDto;
 import mindustrytool.type.StartServerDto;
 import mindustrytool.type.StatsDto;
 import mindustrytool.type.TeamDto;
-import mindustrytool.type.ModDto.ModMetaDto;
 import mindustrytool.type.ServerCommandDto.CommandParamDto;
 import mindustrytool.utils.HudUtils;
 
@@ -254,57 +246,6 @@ public class HttpServer {
                 }
             });
 
-            app.get("maps", context -> {
-                try {
-                    var maps = Vars.customMapDirectory.findAll()
-                            .map(file -> {
-                                try {
-                                    return MapIO.createMap(file, true);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                    return new Map(file, 0, 0, new StringMap(), true, 0, Version.build);
-                                }
-                            }).map(map -> new MapDto()//
-                                    .setName(map.name())//
-                                    .setFilename(map.file.name())
-                                    .setCustom(map.custom)
-                                    .setHeight(map.height)
-                                    .setWidth(map.width));
-
-                    context.contentType(ContentType.JSON);
-                    context.json(maps);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    context.contentType(ContentType.TEXT_PLAIN);
-                    context.status(500);
-                    context.result(e.getMessage());
-                }
-            });
-
-            app.get("mods", context -> {
-                try {
-                    var modFiles = Vars.modDirectory.findAll();
-
-                    var result = new ArrayList<ModDto>();
-                    for (var modFile : modFiles) {
-                        try {
-                            var mod = loadMod(modFile);
-                            result.add(mod);
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    context.contentType(ContentType.JSON);
-                    context.json(result);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    context.contentType(ContentType.TEXT_PLAIN);
-                    context.status(500);
-                    context.result(e.getMessage());
-                }
-            });
-
             System.out.println("Start http server");
             app.start(9999);
             System.out.println("Http server started");
@@ -314,46 +255,6 @@ public class HttpServer {
         System.out.println("Setup http server done");
     }
 
-    private ModDto loadMod(Fi sourceFile) throws Exception {
-        ZipFi rootZip = null;
-
-        try {
-            Fi zip = sourceFile.isDirectory() ? sourceFile : (rootZip = new ZipFi(sourceFile));
-            if (zip.list().length == 1 && zip.list()[0].isDirectory()) {
-                zip = zip.list()[0];
-            }
-
-            ModMeta meta = Vars.mods.findMeta(zip);
-
-            if (meta == null) {
-                Log.warn("Mod @ doesn't have a '[mod/plugin].[h]json' file, skipping.", zip);
-                throw new ModLoadException("Invalid file: No mod.json found.");
-            }
-
-            return new ModDto()//
-                    .setFilename(zip.name())//
-                    .setName(meta.name)
-                    .setMeta(new ModMetaDto()//
-                            .setAuthor(meta.author)//
-                            .setDependencies(meta.dependencies.list())
-                            .setDescription(meta.description)
-                            .setDisplayName(meta.displayName)
-                            .setHidden(meta.hidden)
-                            .setInternalName(meta.internalName)
-                            .setJava(meta.java)
-                            .setMain(meta.main)
-                            .setMinGameVersion(meta.minGameVersion)
-                            .setName(meta.name)
-                            .setRepo(meta.repo)
-                            .setSubtitle(meta.subtitle)
-                            .setVersion(meta.version));
-        } catch (Exception e) {
-            // delete root zip file so it can be closed on windows
-            if (rootZip != null)
-                rootZip.delete();
-            throw e;
-        }
-    }
 
     private synchronized void host(StartServerDto request) {
         if (Vars.state.isGame()) {

50c6275eb2c4cd3bc5029767de6cde7b41641539
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-06-22 13:28:35 +0700

    feat: add unique identifier and timestamp to WorkflowEvent class

--- src/java/mindustrytool/workflow/WorkflowEvent.java
+++ src/java/mindustrytool/workflow/WorkflowEvent.java
@@ -1,12 +1,17 @@
 package mindustrytool.workflow;
 
+import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @RequiredArgsConstructor
 public class WorkflowEvent {
+    private final String id = UUID.randomUUID().toString();
     private final String nodeId;
     private final String name;
     private final Object value;
+
+    private final Long createdAt = System.currentTimeMillis();
 }

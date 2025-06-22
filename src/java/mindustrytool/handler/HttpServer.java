c5fd0000fb2c78bbc0051e36d7b5e28dc3b38e43
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-06-23 01:27:45 +0700

    fix: add keep-alive functionality for SSE clients in HttpServer

--- src/java/mindustrytool/handler/HttpServer.java
+++ src/java/mindustrytool/handler/HttpServer.java
@@ -477,11 +477,11 @@ public class HttpServer {
         });
 
         app.sse("workflow/events", client -> {
-            client.keepAlive();
             client.sendComment("connected");
             client.onClose(() -> controller.workflow.getWorkflowEventConsumers().remove(client));
 
             controller.workflow.getWorkflowEventConsumers().add(client);
+            client.keepAlive();
         });
 
         app.exception(Exception.class, (exception, context) -> {

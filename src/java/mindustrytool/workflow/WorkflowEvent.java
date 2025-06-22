7d39ff13857fa66aa779b568b56c387e59aeed7c
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-06-23 02:13:05 +0700

    fix: add Lombok @Data annotation to WorkflowEvent class for automatic getters and setters

--- src/java/mindustrytool/workflow/WorkflowEvent.java
+++ src/java/mindustrytool/workflow/WorkflowEvent.java
@@ -3,11 +3,13 @@ package mindustrytool.workflow;
 import java.util.Map;
 import java.util.UUID;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
 @ToString
 @RequiredArgsConstructor
+@Data
 public class WorkflowEvent<T extends Map> {
     private final String id = UUID.randomUUID().toString();
     private final String nodeId;

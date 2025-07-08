4e2c1fe00b884c25a0dfafb489156d91811cec0e
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-07-09 02:11:40 +0700

    refactor(ServerController): move DefaultThreadFactory class to end of file
    Improve code organization by moving the inner class to the bottom of the file for better readability and consistency with common Java conventions.

--- src/main/java/mindustrytool/ServerController.java
+++ src/main/java/mindustrytool/ServerController.java
@@ -62,34 +62,6 @@ public class ServerController extends Plugin implements MindustryToolPlugin {
             new SynchronousQueue<Runnable>(),
             new DefaultThreadFactory());
 
-    private class DefaultThreadFactory implements ThreadFactory {
-        private static final AtomicInteger poolNumber = new AtomicInteger(1);
-        private final ThreadGroup group;
-        private final AtomicInteger threadNumber = new AtomicInteger(1);
-        private final String namePrefix;
-
-        @SuppressWarnings("deprecation")
-        DefaultThreadFactory() {
-            @SuppressWarnings("removal")
-            SecurityManager s = System.getSecurityManager();
-            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
-            namePrefix = "background-pool-" +
-                    poolNumber.getAndIncrement() +
-                    "-thread-";
-        }
-
-        public Thread newThread(Runnable r) {
-            Thread t = new Thread(group, r,
-                    namePrefix + threadNumber.getAndIncrement(),
-                    0);
-            if (t.isDaemon())
-                t.setDaemon(false);
-            if (t.getPriority() != Thread.NORM_PRIORITY)
-                t.setPriority(Thread.NORM_PRIORITY);
-            return t;
-        }
-    }
-
     public final ScheduledExecutorService BACKGROUND_SCHEDULER = Executors
             .newSingleThreadScheduledExecutor();
 
@@ -221,5 +193,33 @@ public class ServerController extends Plugin implements MindustryToolPlugin {
     @Override
     public void delete() {
         Log.info("Server controller deleted: " + this);
+    }
+
+    private class DefaultThreadFactory implements ThreadFactory {
+        private static final AtomicInteger poolNumber = new AtomicInteger(1);
+        private final ThreadGroup group;
+        private final AtomicInteger threadNumber = new AtomicInteger(1);
+        private final String namePrefix;
+
+        @SuppressWarnings("deprecation")
+        DefaultThreadFactory() {
+            @SuppressWarnings("removal")
+            SecurityManager s = System.getSecurityManager();
+            group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+            namePrefix = "background-pool-" +
+                    poolNumber.getAndIncrement() +
+                    "-thread-";
+        }
+
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(group, r,
+                    namePrefix + threadNumber.getAndIncrement(),
+                    0);
+            if (t.isDaemon())
+                t.setDaemon(false);
+            if (t.getPriority() != Thread.NORM_PRIORITY)
+                t.setPriority(Thread.NORM_PRIORITY);
+            return t;
+        }
     }
 }

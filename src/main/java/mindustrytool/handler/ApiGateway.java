24d2206c4ba1678de87347f7d9591d95753fea7f
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-07-12 21:25:10 +0700

    refactor(ApiGateway): remove executor from http client builder
    The BACKGROUND_TASK_EXECUTOR is no longer needed for the http client configuration, simplifying the setup.

--- src/main/java/mindustrytool/handler/ApiGateway.java
+++ src/main/java/mindustrytool/handler/ApiGateway.java
@@ -42,7 +42,6 @@ public class ApiGateway {
         this.context = context;
         httpClient = HttpClient.newBuilder()//
                 .connectTimeout(Duration.ofSeconds(2))//
-                .executor(context.get().BACKGROUND_TASK_EXECUTOR)
                 .build();
 
         Log.info("Api gateway handler created: " + this);
